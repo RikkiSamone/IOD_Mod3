c1d8f8d4d67d67a23d11813501d3ee748c696431
Rikki <rikkisamone@gmail.com>
2024-10-26 15:55:19 -0700

    on Q7

--- JSFundamentals3.1.js
+++ JSFundamentals3.1.js
@@ -139,17 +139,13 @@ const inigo = {
         console.log(greeting + this.getCatchPhrase(person));
     },
     
-    getCatchPhrase(person) {
-        if (person += 5) {
-            return 'Nice to meet you.';
-        } else {
-            return 'YOU KILLED MY FATHER! PREPARE TO DIE!';
+    getCatchPhrase:person => person.numFingers === 5
+            ? 'Nice to meet you.'
+            :'YOU KILLED MY FATHER! PREPARE TO DIE!'
         }
     
-    }
-}
 
- inigo.greeting(westley)
+inigo.greeting(westley)
 inigo.greeting(rugen)
 
 
@@ -157,27 +153,47 @@ inigo.greeting(rugen)
 /*7. The following object represents a basketball game and keeps track of the score as the
 game progresses.
 a) Modify each of the methods so that they can be ‘chained’ together and the last line of
-the example code works
+the example code works//
+
+
+
 b) Add a new method to print the full time final score
 c) Add a new object property to keep track of the number of fouls and a method to
 increment it, similar but separate to the score. Include the foul count in the half time and
 full time console messages
 d) Test your object by chaining all the method calls together in different combinations.*/
 
+//a:
+
+
 const basketballGame = {
-score: 0,
-freeThrow() {
-this.score++;
-},
+    score: 0,
+    freeThrow() {
+        this.score++;
+    },
+
 basket() {
-this.score += 2;
-},
+    this.score += 2;
+    },
+
 threePointer() {
-this.score += 3;
-},
+    this.score += 3;
+    },
+
 halfTime() {
-console.log('Halftime score is '+this.score);
+        console.log('Halftime score is '+this.score);
+    }
 }
+
+printThis(){
+    console.log(this);
+    return this; 
 }
+
+printScore() {
+    console.log($(this.score}))
+
+
+
 //modify each of the above object methods to enable function chaining as below:
 basketballGame.basket().freeThrow().freeThrow().basket().threePointer().halfTime();
