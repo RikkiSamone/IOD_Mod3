cb01489842eb074b2a702339687f22b7af3055d5
Rikki <rikkisamone@gmail.com>
2024-10-29 18:09:26 -0700

    initial

---
+++ JSAdvanced.js
@@ -0,0 +1,20 @@
+// Lexical Environment: every function and code block {...} has its own scope, and can define its own lexical (local) environment with its own variables
+let globalVariableCat = 'cat';
+function myFunction() {
+    let localVariableDog = 'dog'
+    return 'global function with local scope variable '+localVariableDog;
+}
+console.log(localVariableDog) // ReferenceError: localVariableDog is not defined
+// globalEnvironment = {
+//     environmentRecord: {
+//        globalVariableCat: 'cat', // has no access to localVariableDog
+//        myFunction: <reference to function object>,
+//    }  
+//    outer: null // there is no parent environment here
+// }
+// localMyFunctionEnvironment = {
+//     environmentRecord: {
+//         localVariableDog: 'dog'
+//     },
+//     outer: globalEnvironment // can still access everything in global
+// }
